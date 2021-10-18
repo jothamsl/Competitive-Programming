64baf700d36074802315f68ed272725dfe5dfc95
Jotham Soboyejo <55670042+jothamsl@users.noreply.github.com>
2021-10-18 19:45:47 +0100

    Changed file struct

--- C++_Language_Features/Template.cc
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h> // Allows us to include entire standart library. e.g iostream, vector
-using namespace std;
-
-int main() {
-  // Solution comes here
-  // Compile the code with the following:
-  // g++ -std=c++11 -o2 -Wall Template.cc -o template
-}
